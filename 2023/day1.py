3386ace98c8bf2e9b7e5f4b5ceacf6b0b91ec71f
Yansen Zhou <hackthehackerman@users.noreply.github.com>
2023-12-01 14:29:38 -0800

    clean up

--- 2023/day1.py
+++ 2023/day1.py
@@ -3,27 +3,11 @@ import re
 from util import tolines
 
 
-input = """
-"""
-
-testinput = """two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen"""
-
-
 def partone():
     lines = tolines("input/day1")
     ret = 0
     for l in lines:
         numbers = re.findall("[0-9]", l)
-        for c in l:
-            if c.isdigit():
-                numbers.append(int(c))
-
         if len(numbers) == 1:
             ret += int(numbers[0]) * 11
         else:
